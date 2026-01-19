10f704ed72e03f2af8627e7cecce23d22dabe405
Pranay Mahendrakar <mahendrakarpranay@gmail.com>
2026-01-19 12:43:07 +0530

    Initial commit

---
+++ 2025-12-29 12.49.13 - Accepted - runtime 2ms - memory 57.7MB.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+  * @param {number} k
+   * @return {number}
+    */
+    var findKOr = function(nums, k) {
+        let result = 0;
+            for (let bit = 0; bit < 32; bit++) {
+                    let count = 0;
+                            for (const num of nums) {
+                                        if ((num >> bit) & 1) count++;
+                                                }
+                                                        if (count >= k) result |= (1 << bit);
+                                                            }
+                                                                return result;
+                                                                }
